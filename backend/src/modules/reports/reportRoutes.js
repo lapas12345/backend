984483c14e621bebd2d83d476209c4279bbb6de4
lapas12345 <juanzamvera@gmail.com>
2026-05-30 20:03:08 -0500

    Delete backend directory

--- backend/src/modules/reports/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./pdfControllador');
-
-router.get('/periods', controller.getPeriods);
-router.post('/generate', controller.generateReport);
-
-module.exports = router;
